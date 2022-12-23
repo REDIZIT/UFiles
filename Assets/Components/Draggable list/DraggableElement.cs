e72cd500b40acb00bd74db270d5a328fcbe1f0e6
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2022-12-23 23:03:53 +0300

    Rename window template

---
+++ Assets/Components/Draggable list/DraggableElement.cs
@@ -0,0 +1,32 @@
+using Mono.Cecil.Cil;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace InApp.DraggableList
+{
+    public class DraggableElement : MonoBehaviour
+    {
+        public int index;
+        public DraggableList list;
+
+        [SerializeField] private Transform ui;
+
+        private RectTransform rect;
+
+        private void Awake()
+        {
+            rect = GetComponent<RectTransform>();
+        }
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (RectTransformUtility.RectangleContainsScreenPoint(rect, Input.mousePosition))
+                {
+                    list.OnDragBegin(this);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Components/Draggable list/DraggableList.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace InApp.DraggableList
+{
+    public class DraggableList : MonoBehaviour
+    {
+        [SerializeField] private Transform content, dragContent;
+        [SerializeField] private Transform dragPlaceholder;
+
+        [SerializeField] private DraggableElement prefab;
+        
+        private DraggableElement draggingItem;
+        private int startIndex;
+
+        private void Start()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var inst = Instantiate(prefab, content);
+                inst.index = i;
+                inst.list = this;
+            }
+        }
+        private void Update()
+        {
+            if (draggingItem != null)
+            {
+                draggingItem.transform.position = Input.mousePosition;
+
+                float deltaY = draggingItem.transform.position.y - dragPlaceholder.position.y;
+                float yPerElement = 24 + 2;
+
+                int index = startIndex + (int)(deltaY / yPerElement);
+
+                dragPlaceholder.transform.SetSiblingIndex(index);
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    StopDrag();
+                }
+            }
+        }
+        public void OnDragBegin(DraggableElement element)
+        {
+            element.transform.parent = dragContent;
+            dragPlaceholder.gameObject.SetActive(true);
+            dragPlaceholder.SetSiblingIndex(element.index);
+            startIndex = element.index;
+            draggingItem = element;
+        }
+        private void StopDrag()
+        {
+            int index = dragPlaceholder.GetSiblingIndex();
+            dragPlaceholder.gameObject.SetActive(false);
+            draggingItem.transform.parent = content;
+            draggingItem.transform.SetSiblingIndex(index);
+            draggingItem = null;
+        }
+    }
+}
