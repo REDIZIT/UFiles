5d26948f42980fc7d3481a974087f7b54dadbe40
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2023-01-13 21:57:14 +0300

    Graphy and console

--- Assets/Scripts/Bridge/BridgeLauncher.cs
+++ Assets/Scripts/Bridge/BridgeLauncher.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Pipes;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
+using Debug = UnityEngine.Debug;
 
 namespace InApp
 {
@@ -15,59 +18,86 @@ namespace InApp
         [SerializeField] private RawImage image;
 
         private TcpClient client;
+        private Thread t;
 
         void Start ()
         {
-            DateTime t = DateTime.Now;
-            StartBridge();
-            StartTCP();
-            UnityEngine.Debug.Log("Taken: " + (DateTime.Now - t).TotalMilliseconds + "ms");
+            t = new Thread(StartBridge);
+            t.Start();
         }
         private void OnApplicationQuit()
         {
             client.Close();
+            t.Abort();
         }
 
         private void StartBridge()
+        {
+            Stopwatch w = Stopwatch.StartNew();
+
+            StartProcess();
+
+            w.Stop();
+            Log("Start time: " + w.ElapsedMilliseconds);
+            w.Restart();
+
+            StartTCP();
+
+            Log("TCP time: " + w.ElapsedMilliseconds);
+        }
+        private void StartProcess()
         {
             string exePath = Application.streamingAssetsPath + "/Release/net7.0/UFilesBridge.exe";
             Process.Start(exePath);
         }
         private void StartTCP()
         {
-            client = new TcpClient("localhost", 209);
+            Stopwatch w = Stopwatch.StartNew();
 
-            UnityEngine.Debug.Log("Connected");
+            client = new TcpClient();
+
+            Log("Connected");
+
+            client.Connect(new IPAddress(new byte[] { 127, 0, 0, 1 }), 1703);
+
+            Log("Connected in " + w.ElapsedMilliseconds);
+            w.Restart();
 
             var stream = client.GetStream();
             string path = "C:/Test/1.txt";
             stream.Write(Encoding.UTF8.GetBytes(path), 0, path.Length);
 
+            Log("Sent command in " + w.ElapsedMilliseconds);
+            w.Restart();
+
             byte[] buffer = new byte[1024 * 4];
-            int bytesCount = stream.Read(buffer, 0, buffer.Length);
+            stream.Read(buffer, 0, buffer.Length);
 
-            //byte[] bytes = ReceiveAll(client.Client);
+            Log("Received command in " + w.ElapsedMilliseconds);
+            w.Restart();
 
-            UnityEngine.Debug.Log("Bytes count = " + bytesCount);
 
-            Texture2D tex = new Texture2D(1, 1);
-            tex.LoadImage(buffer);
+            
 
-            image.texture = tex;
-        }
-        private byte[] ReceiveAll(Socket socket)
-        {
-            var buffer = new List<byte>();
-            while (socket.Available > 0)
+            Log("Texture2D created in " + w.ElapsedMilliseconds);
+            w.Restart();
+
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
             {
-                var currByte = new Byte[1];
-                var byteCounter = socket.Receive(currByte, currByte.Length, SocketFlags.None);
-                if (byteCounter.Equals(1))
-                {
-                    buffer.Add(currByte[0]);
-                }
-            }
-            return buffer.ToArray();
+                Texture2D tex = new Texture2D(1, 1);
+                tex.LoadImage(buffer);
+
+                image.texture = tex;
+            });
+
+            Log("Image set in " + w.ElapsedMilliseconds);
+        }
+        private void Log(string messsage)
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                Debug.Log(messsage);
+            });
         }
     }
     public class TestCommand
